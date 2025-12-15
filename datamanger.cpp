2bda6355b8ca2356f1a3542eb0e06a0e03657f6f
yousef <ym01284249631@gmail.com>
2025-12-15 19:56:30 +0200

    Fix user data entries in createDefaultData function

--- datamanger.cpp
+++ datamanger.cpp
@@ -11,27 +11,27 @@ QString g_jsonFileName = "users_data.json";
 void createDefaultData() {
     g_allUsers = {
         {
-            "30503310101571", "leader", "Yousef Mohamed Ibrahim", "El Salam", "31/3/2005", "2224900099", "Male", "30503310101571", "1st year , Electrical", ":/image/image/leader.jpg", ":/image/image/gdol5.png", ":/image/image/finalgdol.png","1/9/2025", 5078, "Grievances", 150, " ", "nothing",
+            "305033101015", "leader", "Yousef Mohamed Ibrahim", "El Salam", "31/3/2005", "22249000", "Male", "305033101015", "1st year , Electrical", ":/image/image/leader.jpg", ":/image/image/gdol5.png", ":/image/image/finalgdol.png","1/9/2025", 5078, "Grievances", 150, " ", "nothing",
             "Programming", "Maths", "Discrete", "Circuits", "Solid State",
             80, 60, 50, 70, 50
         },
         {
-            "30606010111838", "shado", "Shady Esmat Ahmed", "El Salam", "1/6/2006", "2224900309", "Male", "30606010111838", "1st year , Medical", ":/image/image/babyshado.jpg", ":/image/image/gdol2",":/image/image/finalgdol1.png", "1/10/2025", 3050, "College expenses", 3750, " ", "nothing",
+            "306060101118", "shado", "Shady Esmat Ahmed", "El Salam", "1/6/2006", "22249003", "Male", "306060101118", "1st year , Medical", ":/image/image/babyshado.jpg", ":/image/image/gdol2",":/image/image/finalgdol1.png", "1/10/2025", 3050, "College expenses", 3750, " ", "nothing",
             "Programming", "Maths", "Discrete", "Circuits", "Solid State",
             50, 51, 65, 98, 74
         },
         {
-            "30702188800679", "hegzo", "Yousef Ali Hegazi", "Faisal", "18/2/2007", "2224900338", "Male", "30702188800679", "1st year , Computer", ":/image/image/yousef3sool.jpg", ":/image/image/gdol.png",":/image/image/finalgdol2.png", "15/9/2025", 5492, "Administrative fees", 500, " ", "nothing",
+            "307021888006", "hegzo", "Yousef Ali Hegazi", "Faisal", "18/2/2007", "22249003", "Male", "307021888006", "1st year , Computer", ":/image/image/yousef3sool.jpg", ":/image/image/gdol.png",":/image/image/finalgdol2.png", "15/9/2025", 5492, "Administrative fees", 500, " ", "nothing",
             "Programming", "Maths", "Discrete", "Circuits", "Solid State",
             100, 70, 85, 75, 95
         },
         {
-            "30608150102513", "dep", "Seif Mohamed Fawzy", "Alf Maskan", "15/8/2006", "2224900324", "Male", "30608150102513", "Preparation year", ":/image/image/littledep.jpg", ":/image/image/gdol4.png", ":/image/image/finalgdol3.png", "25/9/2025", 2594, "Fees", 1500, " ", "nothing",
+            "306081501025", "dep", "Seif Mohamed Fawzy", "Alf Maskan", "15/8/2006", "22249003", "Male", "306081501025", "Preparation year", ":/image/image/littledep.jpg", ":/image/image/gdol4.png", ":/image/image/finalgdol3.png", "25/9/2025", 2594, "Fees", 1500, " ", "nothing",
             "Programming", "Maths", "Discrete", "Circuits", "Solid State",
             54, 45, 99, 62, 62
         },
         {
-            "30606060107831", "bob", "Basel Saeed Salah", "Madinet Nasr", "6/6/2006", "2224900254", "Male", "30606060107831", "1st year , Communication", ":/image/image/babyyassin.jpg", ":/image/image/gdol3.png", ":/image/image/finalgdol4.png", "20/10/2025", 8910, "Tuition fees", 3500, " ", "nothing",
+            "306060601078", "bob", "Basel Saeed Salah", "Madinet Nasr", "6/6/2006", "22249002", "Male", "306060601078", "1st year , Communication", ":/image/image/babyyassin.jpg", ":/image/image/gdol3.png", ":/image/image/finalgdol4.png", "20/10/2025", 8910, "Tuition fees", 3500, " ", "nothing",
             "Programming", "Maths", "Discrete", "Circuits", "Solid State",
             54, 45, 88, 99, 55
         },
